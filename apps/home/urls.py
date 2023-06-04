4c9cefdb0a4d8674ef9b2c8aafa393669142039c
Aditya Prakash Gupta <adityaprakashgupta726@gmail.com>
2023-06-04 19:26:03 +0530

    Initial Prototype

---
+++ apps/home/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from apps.home import views
+
+app_name = "home"
+
+urlpatterns = [
+	path("", views.index, name="index"),
+	path("about", views.about, name="about"),
+	path("services", views.services, name="services"),
+	path("contact", views.contact, name="contact"),
+
+]
